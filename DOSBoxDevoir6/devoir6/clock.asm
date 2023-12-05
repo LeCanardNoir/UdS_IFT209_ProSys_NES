86af78747bb76c259af3f9c9c01fd7061c4827e7
LeCanardNoir <dev.lecanardnoir@gmail.com>
2023-12-04 19:02:51 -0500

    PROGRESS: couleur du background

--- DOSBoxDevoir6/devoir6/clock.asm
+++ DOSBoxDevoir6/devoir6/clock.asm
@@ -1,5 +1,5 @@
 
-;Déclarations nécessaires à l'en-tête du fichier compilé en format iNes.
+;Dï¿½clarations nï¿½cessaires ï¿½ l'en-tï¿½te du fichier compilï¿½ en format iNes.
 	
  .inesprg 1			;Nombre de banques de PRG-ROM: 1
  .ineschr 1			;Nombre de banques de CHR-ROM: 1
@@ -9,14 +9,14 @@
  
 
 
-;Memoire des sprites.  Le segment $700 - $7FF est entierement dédié aux sprites.
+;Memoire des sprites.  Le segment $700 - $7FF est entierement dï¿½diï¿½ aux sprites.
  .bss
 
 
-CLOCK.DIGIT.Y	=	$700		;Premier octet: coordonnée Y
-CLOCK.DIGIT.T	=	$701		;second octet: numéro de tuile
-CLOCK.DIGIT.S	=	$702		;troisième octet: attributs
-CLOCK.DIGIT.X	=	$703		;quatrième octet: coordonnée X
+CLOCK.DIGIT.Y	=	$700		;Premier octet: coordonnï¿½e Y
+CLOCK.DIGIT.T	=	$701		;second octet: numï¿½ro de tuile
+CLOCK.DIGIT.S	=	$702		;troisiï¿½me octet: attributs
+CLOCK.DIGIT.X	=	$703		;quatriï¿½me octet: coordonnï¿½e X
 
 
 
@@ -25,95 +25,95 @@ CLOCK.COUNTER = $00
 
 
  .code							;Segment de code
- .org $c000						;débutant à l'adresse $C000 (banque PRG #0)
+ .org $c000						;dï¿½butant ï¿½ l'adresse $C000 (banque PRG #0)
 
-	;Fonction main.  Appelée lors d'une interruption Reset.  Initialisations suivies d'une boucle infinie.
+	;Fonction main.  Appelï¿½e lors d'une interruption Reset.  Initialisations suivies d'une boucle infinie.
 
 Main:
 	sei							;Interdit les interruptions IRQ et BRK
- 	cld							;Mode décimal désactivé
- 	ldx #$ff					;Prépare le pointeur de pile...
-	txs							;Pointeur de pile initilisé au dessus de la pile
+ 	cld							;Mode dï¿½cimal dï¿½sactivï¿½
+ 	ldx #$ff					;Prï¿½pare le pointeur de pile...
+	txs							;Pointeur de pile initilisï¿½ au dessus de la pile
  	inx							;place la valeur 0 dans X
-	stx $2000					;Initialise le PPU temporairement: NMI désactivées
+	stx $2000					;Initialise le PPU temporairement: NMI dï¿½sactivï¿½es
 	
-	stx	<CLOCK.COUNTER			;Initialise le compteur de l'horloge à 0
+	stx	<CLOCK.COUNTER			;Initialise le compteur de l'horloge ï¿½ 0
 
- 	jsr InitSpriteMem			;Mise à zéro du segment de mémoire contenant les sprites
+ 	jsr InitSpriteMem			;Mise ï¿½ zï¿½ro du segment de mï¿½moire contenant les sprites
 	jsr palette					;fonction qui initialise les palettes 
 	
 	
-	lda	#1						;Initialise le no de tuile à 1
+	lda	#1						;Initialise le no de tuile ï¿½ 1
 	sta	CLOCK.DIGIT.T			;...
-	lda	#120					;Positionne le chiffre à (128,120)
+	lda	#120					;Positionne le chiffre ï¿½ (128,120)
 	sta	CLOCK.DIGIT.Y			;...
 	lda	#128					;...
 	sta	CLOCK.DIGIT.X			;...
-	lda	#0						;Les attributs sont  à 0 : palette 0 et aucune transformation
+	lda	#0						;Les attributs sont  ï¿½ 0 : palette 0 et aucune transformation
 	sta	CLOCK.DIGIT.S			;....
 	
 	
-	jsr		wait_vblank			;Attend une période de VBlank avant de faire des modifications
+	jsr		wait_vblank			;Attend une pï¿½riode de VBlank avant de faire des modifications
 	
-	lda		#$07				;Le segment de la mémoire choisi est $0700 à $07FF
-	sta		$4014				; Mise à jour de la mémoire des sprites par DMA
+	lda		#$07				;Le segment de la mï¿½moire choisi est $0700 ï¿½ $07FF
+	sta		$4014				; Mise ï¿½ jour de la mï¿½moire des sprites par DMA
 	
 	;Initialise le PPU:
 	; ($2000)
-	;-Interruptions NMI activées
+	;-Interruptions NMI activï¿½es
 	;-Grandeur des sprites: 8x8
-	;-table de schémas pour l'arrière-plan: #1
-	;-table de schémas pour les sprites: #0
-	;-incrémentation d'index : +1
-	;-numéro de table de noms affichée: 0
+	;-table de schï¿½mas pour l'arriï¿½re-plan: #1
+	;-table de schï¿½mas pour les sprites: #0
+	;-incrï¿½mentation d'index : +1
+	;-numï¿½ro de table de noms affichï¿½e: 0
 
 	;($2001)
-	;-aucune modification d'intensité des couleurs
+	;-aucune modification d'intensitï¿½ des couleurs
 	;-sprites visibles
-	;-arrière-plan invisible
-	;-première colonne invisible pour les sprites et l'arrière-plan
+	;-arriï¿½re-plan invisible
+	;-premiï¿½re colonne invisible pour les sprites et l'arriï¿½re-plan
 	;-affichage couleur
 
 	
 	lda #%00010000	;place la valeur binaire 00010000 dans A
-	sta $2001		;écrit dans le registre de contrôle #2
+	sta $2001		;ï¿½crit dans le registre de contrï¿½le #2
 	lda #%10001000	;place la valeur binaire 10001000 dans A
-	sta $2000		;écrit dans le registre de contrôle #1
+	sta $2000		;ï¿½crit dans le registre de contrï¿½le #1
 	
 mainEnd:
 	jmp mainEnd		;Boucle infinie
 
 
 
-	;Fonction qui attend une période de VBlank
+	;Fonction qui attend une pï¿½riode de VBlank
 
 wait_vblank:
- 	bit $2002				;teste les bits du registre d'état du PPU
- 	bpl wait_vblank			;tant que le bit 7 est éteint (signe = 0), on boucle
+ 	bit $2002				;teste les bits du registre d'ï¿½tat du PPU
+ 	bpl wait_vblank			;tant que le bit 7 est ï¿½teint (signe = 0), on boucle
 	
 	rts						;fin	
 
 	
 	
-	;Fonction qui met à zéro tous les 256 octets du segment réservé aux sprites
+	;Fonction qui met ï¿½ zï¿½ro tous les 256 octets du segment rï¿½servï¿½ aux sprites
 
 InitSpriteMem:
  	lda #0			;initialise A avec 0
 	tay				;initialise Y avec 0 aussi
 
 init00:
-	sta $700,y		;écrit 0 à $700 + Y
-	iny				;incrémente Y
+	sta $700,y		;ï¿½crit 0 ï¿½ $700 + Y
+	iny				;incrï¿½mente Y
 	bne init00		;lorsque Y redevient 0 (255 + 1 = 0), fin	
 	
 	rts				;fin
 
 
 
-	;segment de données contenant les numéros de couleur des palettes
+	;segment de donnï¿½es contenant les numï¿½ros de couleur des palettes
 paldata:
 
-; Palette d'arrière-plan.  Tout est noir sauf la première palette
+; Palette d'arriï¿½re-plan.  Tout est noir sauf la premiï¿½re palette
 	.byte	$0f,$20,$10,$00	;Tons de gris, pratique pour voir les tuiles en debug
 	.byte	$0f,$0f,$0f,$0f	;Noir
 	.byte	$0f,$0f,$0f,$0f	;Noir
@@ -127,57 +127,57 @@ paldata:
 
 	;fonction qui initialise les palettes.
 palette:
-	jsr wait_vblank		;attend une période de VBlank avant d'écrire
+	jsr wait_vblank		;attend une pï¿½riode de VBlank avant d'ï¿½crire
 
-	lda #$3f			;initialise le registre d'index de la mémoire vidéo
-	sta $2006			;avec l'adresse $3F00 : mémoire de palettes
+	lda #$3f			;initialise le registre d'index de la mï¿½moire vidï¿½o
+	sta $2006			;avec l'adresse $3F00 : mï¿½moire de palettes
 	lda #0				;...
 	sta $2006			;...
  
 	ldx #0				;index de lecture des valeurs de palette
 
 do_palette:
-	lda paldata,x		;obtient un numéro de couleur
-	sta $2007			;écrit le numéro de couleur dans le registre de données
-	inx					;incrémente l'index
-	cpx #32				;a-t-on écrit les 32 couleurs?
+	lda paldata,x		;obtient un numï¿½ro de couleur
+	sta $2007			;ï¿½crit le numï¿½ro de couleur dans le registre de donnï¿½es
+	inx					;incrï¿½mente l'index
+	cpx #32				;a-t-on ï¿½crit les 32 couleurs?
 	bne do_palette		;sinon, continue
 	
 	rts					;fin
 
 
 
-	;Routine d'interruption pour NMI: Met à jour le compteur et le sprite à l'écran quand une seconde s'est écoulée.
-	;Aucune sauvegarde des registres n'est fait parce que le Main ne les utilise plus lorsque les interruptions ont activées.
+	;Routine d'interruption pour NMI: Met ï¿½ jour le compteur et le sprite ï¿½ l'ï¿½cran quand une seconde s'est ï¿½coulï¿½e.
+	;Aucune sauvegarde des registres n'est fait parce que le Main ne les utilise plus lorsque les interruptions ont activï¿½es.
 NMI_InterruptRoutine:   
 
-			clc							;Place le report à 0
-			lda		<CLOCK.COUNTER		;Récupère le compteur
-			adc		#1					;incrémente le compteur
-			sta		<CLOCK.COUNTER		;met à jour la variable
-			sec							;Place le report à 1 (et donc l'emprunt à 0)
+			clc							;Place le report ï¿½ 0
+			lda		<CLOCK.COUNTER		;Rï¿½cupï¿½re le compteur
+			adc		#1					;incrï¿½mente le compteur
+			sta		<CLOCK.COUNTER		;met ï¿½ jour la variable
+			sec							;Place le report ï¿½ 1 (et donc l'emprunt ï¿½ 0)
 			sbc		#60					;compare avec 60
-			bne		NMI_fin				;si on n'a pas atteint 60, rien à faire
+			bne		NMI_fin				;si on n'a pas atteint 60, rien ï¿½ faire
 			
-			;CLOCK.COUNTER == 60: il faut mettre à jour le chiffre affiché
+			;CLOCK.COUNTER == 60: il faut mettre ï¿½ jour le chiffre affichï¿½
 			
 			lda		#0					
-			sta		<CLOCK.COUNTER		;replace le compteur à 0
-			clc							;Place le report à 0
+			sta		<CLOCK.COUNTER		;replace le compteur ï¿½ 0
+			clc							;Place le report ï¿½ 0
 			lda		#1					;Acc = 1
 			adc		CLOCK.DIGIT.T		;Acc += No. de tuile
-			sta		CLOCK.DIGIT.T		;Met à jour le no de tuile
+			sta		CLOCK.DIGIT.T		;Met ï¿½ jour le no de tuile
 			sec							;emprunt = 0
-			sbc		#11					;a-t-on dépassé la tuile no 10 (caractère '9') ?
-			bne		NMI_10				;non, mise à jour des sprites
+			sbc		#11					;a-t-on dï¿½passï¿½ la tuile no 10 (caractï¿½re '9') ?
+			bne		NMI_10				;non, mise ï¿½ jour des sprites
 			
-			;No de tuile plus grand que 10: il faut retourner à la tuile no 1 (caractère '0')
+			;No de tuile plus grand que 10: il faut retourner ï¿½ la tuile no 1 (caractï¿½re '0')
 			
 			lda		#1
 			sta		CLOCK.DIGIT.T		;no de tuile = 1
 			
-NMI_10:		lda		#$07				;Le segment de la mémoire choisi est $0700 à $07FF
-			sta		$4014				; Mise à jour de la mémoire des sprites par DMA
+NMI_10:		lda		#$07				;Le segment de la mï¿½moire choisi est $0700 ï¿½ $07FF
+			sta		$4014				; Mise ï¿½ jour de la mï¿½moire des sprites par DMA
 		
 NMI_fin:	rti 						;fin de l'interruption.
 
@@ -191,11 +191,11 @@ IRQ_BRK_InterruptRoutine:
 
 			
  .bank 1
- .org $fffa								;Positionne le compteur d'emplacement à $FFFA, le début des vecteurs d'interruption
+ .org $fffa								;Positionne le compteur d'emplacement ï¿½ $FFFA, le dï¿½but des vecteurs d'interruption
  .word NMI_InterruptRoutine				;Premier segment de 16 bits, adresse de la routine NMI ($FFFA, $FFFB)
  .word Main								;Second segment de 16 bits, adresse de la routine BREAK ($FFFC, $FFFD)
  .word IRQ_BRK_InterruptRoutine;			;Dernier segment de 16 bits, adresse de la routine BREAK ($FFFE, $FFFF)
  
  .bank 2
- .org 0									;Positionne le compteur au début de la banque #2
- .incbin "smbvrom.nes"					;Remplit la banque avec le fichier contenant les schémas des tuiles
+ .org 0									;Positionne le compteur au dï¿½but de la banque #2
+ .incbin "smbvrom.nes"					;Remplit la banque avec le fichier contenant les schï¿½mas des tuiles
